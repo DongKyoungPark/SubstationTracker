53421e3225d321a0c3f81942c58ec017dc780ec5
DongKyoung <oe1lolo@nate.com>
2025-03-06 12:50:25 +0900

    feat: 구독 종료시점 메일 알림 서비스

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import { PORT } from './config/env.js';
+
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(arcjetMiddleware);
+
+app.use('/api/vi/auth', authRouter);
+app.use('/api/vi/users', userRouter);
+app.use('/api/vi/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+app.listen(PORT, async () => {
+  console.log(
+    `🚀 Subscription Tracker API is running on http://localhost:${PORT}`
+  );
+
+  await connectToDatabase();
+});
+
+export default app;
